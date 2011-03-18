0af8fce3cc1f32ef763602b57c38a0ae85090b06
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2011-03-18 16:12:47 +0000

    much more umlaut support

--- src/pandoc-lit.hs
+++ src/pandoc-lit.hs
@@ -657,17 +657,81 @@ avoidUTF8 = concatMap f where
             else encodeCharForLatex c
 
 encodeCharForLatex c = case fromEnum c of
-  0x00E4  ->  "\\\"a"
-  0x00EB  ->  "\\\"e"
-  0x00EF  ->  "\\\"i"
-  0x00F6  ->  "\\\"o"
-  0x00FC  ->  "\\\"u"
-  0x00C4  ->  "\\\"A"
-  0x00CB  ->  "\\\"E"
-  0x00CF  ->  "\\\"I"
-  0x00D6  ->  "\\\"O"
-  0x00DC  ->  "\\\"U"
+  -- LATIN EXTENDED
+  0x00C0  ->  "\\`{A}"
+  0x00C1  ->  "\\'{A}"
+  0x00C2  ->  "\\^{A}"
+  0x00C3  ->  "\\~{A}"
+  0x00C4  ->  "\\\"{A}"
+  0x00C5  ->  "\\r{A}"
+  0x00C6  ->  "\\AE " 
+  0x00C7  ->  "\\c{C}"
+  0x00C8  ->  "\\`{E}"
+  0x00C9  ->  "\\'{E}"
+  0x00CA  ->  "\\^{E}"
+  0x00CB  ->  "\\\"{E}"
+  0x00CC  ->  "\\`{I}"
+  0x00CD  ->  "\\'{I}"
+  0x00CE  ->  "\\^{I}"
+  0x00CF  ->  "\\\"{I}"
+  
+  -- 0x00D0  -> -- What is this?
+  0x00D1  ->  "\\~{N}"
+  0x00D2  ->  "\\`{O}"
+  0x00D3  ->  "\\'{O}"
+  0x00D4  ->  "\\^{O}"
+  0x00D5  ->  "\\~{O}"
+  0x00D6  ->  "\\\"{O}"
+  -- 0x00D7  ->  -- What is this?
+  0x00D8  ->  "\\O "
+  0x00D9  ->  "\\`{U}"
+  0x00DA  ->  "\\'{U}"
+  0x00DB  ->  "\\^{U}"
+  0x00DC  ->  "\\\"{U}"
+  0x00DD  ->  "\\'{Y}"
+  -- 0x00DE  ->  -- What is this?
+  0x00DF  ->  "\\ss "
+  
+  0x00E0  ->  "\\`{a}"
+  0x00E1  ->  "\\'{a}"
+  0x00E2  ->  "\\^{a}"
+  0x00E3  ->  "\\~{a}"
+  0x00E4  ->  "\\\"{a}"
+  0x00E5  ->  "\\r{a}"
+  0x00E6  ->  "\\ae " 
+  0x00E7  ->  "\\c{c}"
+  0x00E8  ->  "\\`{e}"
+  0x00E9  ->  "\\'{e}"
+  0x00EA  ->  "\\^{e}"
+  0x00EB  ->  "\\\"{e}"
+  0x00EC  ->  "\\`{\\i}"
+  0x00ED  ->  "\\'{\\i}"
+  0x00EE  ->  "\\^{\\i}"
+  0x00EF  ->  "\\\"{\\i}"
+  -- 0x00F0  -> -- What is this?
+  0x00F1  ->  "\\~{n}"
+  0x00F2  ->  "\\`{o}"
+  0x00F3  ->  "\\'{o}"
+  0x00F4  ->  "\\^{o}"
+  0x00F5  ->  "\\~{o}"
+  0x00F6  ->  "\\\"{o}"
+  -- 0x00F7  ->  -- What is this?
+  0x00F8  ->  "\\o "
+  0x00F9  ->  "\\`{u}"
+  0x00FA  ->  "\\'{u}"
+  0x00FB  ->  "\\^{u}"
+  0x00FC  ->  "\\\"{u}"
+  0x00FD  ->  "\\'{y}"
+  -- 0x00FE  ->  -- What is this?
+  0x00FF  ->  "\\\"{y} "
+  
+  -- GREEK
   0x03BB  ->  "\\ensuremath{\\lambda}"
+  
+  -- GENERAL INTERPUNCTUATION
+  0x2013  ->  "--"
+  
+  -- all others
   code    ->  "{\\char" ++ show code ++ "}"
 
 transformEval :: Config -> FilePath -> String -> IO String
